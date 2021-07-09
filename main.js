47f00edb485c73bba02b21a71905d14113aa9980
RidhoCode <46701110+RidhoCode01@users.noreply.github.com>
2021-07-09 21:02:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const textElement = ['Pelajar', 'Coder',  'Blogger'];
+let count = 0;
+let txtIndex = 0;
+let current = '';
+let words = '';
+
+(function teks() {
+  if(count == textElement. 
+  length){
+    count = 0;
+  }
+  currentTxt = textElement[count];
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector('.teks-ubah').textContent = words;
+  if(words.length == currentTxt.length){
+    count++;
+    txtIndex = 0;
+  }
+  setTimeout(teks, 500);
+})();
